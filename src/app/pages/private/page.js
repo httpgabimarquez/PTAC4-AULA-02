91640fbd3a877a900fc8c0edf3edf386aa023eb4
httpgabimarquez <gabrielamarquezzzz6@gmail.com>
2023-08-01 15:56:35 -0400

    Insercao do remove

--- src/app/pages/private/page.js
+++ src/app/pages/private/page.js
@@ -3,7 +3,9 @@ import Cookies from "js-cookie";
 import { useRouter } from 'next/navigation';
 
 export default function PagePrivate() {
-   
+    const handlerGenerationToken = () => {
+        Cookies.remove('token')
+      };
     return (
         <div>
             <h1>Página Privada</h1>
